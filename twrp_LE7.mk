5b53987641498f253fafae8e748def8cc56903a2
RFlVQAZ <149435833+RFlVQAZ@users.noreply.github.com>
2023-11-10 00:48:52 +0300

    Update twrp_LE7.mk

--- twrp_LE7.mk
+++ twrp_LE7.mk
@@ -6,13 +6,13 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/LE7/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/LE7/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from TECNO-LE7 device
+# Inherit from LE7 device
 $(call inherit-product, device/tecno/LE7/device.mk)
 
 PRODUCT_DEVICE := LE7
